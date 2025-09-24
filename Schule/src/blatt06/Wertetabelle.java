85d1bf7ed087e0cb04545970a4a4135370bdd9a2
TimSupa <tim.haggerty02@gmail.com>
2025-09-24 11:21:13 +0200

    hmh

---
+++ Schule/src/blatt06/Wertetabelle.java
@@ -0,0 +1,24 @@
+package blatt06;
+
+import java.util.Scanner;
+
+public class Wertetabelle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x[] = new int[];
+        System.out.printf("Bis zu welchem Grad wollen Sie Ausgaben haben?");
+        int grad = input.nextInt();
+        double koeff[] = new double[grad + 1];
+        for (int i = grad; i != -1 ; i--) {
+            System.out.printf("Was ist ihr erster Koeffizient?");
+            koeff[i] = input.nextDouble();
+        }
+        for (int i = grad; i != -1; i--) {
+            System.out.printf("Was ist ihr erster X-Wert");
+
+        }
+
+
+
+    }
+}
