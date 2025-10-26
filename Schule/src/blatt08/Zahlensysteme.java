75ca568f82e13255ea07020daacf80f1b0c87019
Timsupa <tim.haggerty02@gmail.com>
2025-10-26 20:40:04 +0100

    arbeit blatt 06

--- Schule/src/blatt08/Zahlensysteme.java
+++ Schule/src/blatt08/Zahlensysteme.java
@@ -1,10 +1,39 @@
 package blatt08;
 
-public class Zahlensysteme {
-    public static int istBinär(int a){
-        boolean istEs;
-        String intString = new String(Integer.toString(a));
-
+public static boolean istDezimal(int a){
+    boolean istEs = false;
+    String intString = new String(Integer.toString(a));
+    if(intString.contains("(")) {
+        if("2".equals(intString.charAt(intString.length()))) {
+            istEs = true;
+        }else {
+            istEs = false;
         }
+    }else{
+        istEs = false;
     }
+    return istEs;
+}
+public static boolean istDezimal(String a){
+    boolean istEs = false;
+    String intString = new String(a);
+    if(intString.contains("(")) {
+        istEs = false;
+    }else {
+        istEs = true;
+    }
+    return istEs;
+}
+public static boolean istHexadezimal(int a){
+    boolean istEs = false;
+    String intString = new String(Integer.toString(a));
+    if(intString.contains("(")) {
+        if("6".equals(intString.charAt(intString.length()))&&"1".equals(intString.charAt(intString.length()-1))) {
+            istEs = true;
+        }
+    }else  {
+        istEs = false;
+    }
+    return istEs;
+}
 
