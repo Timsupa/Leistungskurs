7c3f6b5b8e2296b1de14e231e3416d69e620b5f6
Timsupa <tim.haggerty02@gmail.com>
2026-01-12 14:30:14 +0100

    arbeit blatt 10

--- Schule/src/blatt15/Kaese.java
+++ Schule/src/blatt15/Kaese.java
@@ -1,12 +1,44 @@
 package blatt15;
 import schisch_visualizer.*;
+import blatt14.Simulation;
 public class Kaese {
     public static void wasser (char[][] käs) {
+        boolean randwe = false;
+        boolean randsü = false;
+        boolean randddr = false;
+        int temp = 0;
+        int temp2 = 0;
+        int g = 0;
         for (int i = 0; 10 > i;i++){
-            for (int g = 0; 10 > g; g++){
-                if (käs[i][g] == '0')
+                if (käs[i][g] == '0'){
                     käs[i][g] = '2';
-            }
+                }
+                if (g == 10){
+                    randsü = true;
+                }else{
+                    randsü = false;
+                }
+                if (i == 10){
+                    randddr = true;
+                }else{
+                    randddr = false;
+                }
+                if(i == 0){
+                    randwe = true;
+                }else{
+                    randwe = false;
+                }
+                temp = g;
+                temp2 = i;
+                while (Simulation.getSueden(käs,randsü,i,g) != '0' && Simulation.getWesten(käs,randwe,i,g) != '0' && Simulation.getOsten(käs,randddr,i,g) != '0'){
+                    if (Simulation.getSueden(käs,randsü,i,g) != '0'){
+                        g++;
+                        käs[i][g] = '2';
+                        g = temp;
+                    }
+                    if (Simulation.getOsten(käs,randddr,i,g) != '0'){
+
+                    }}
         }
     }
     public static void main(String[] args) {
@@ -27,7 +59,6 @@ public class Kaese {
                 }
             }
         }
-
         sv.step(kaesU);
         sv.start();
     }
