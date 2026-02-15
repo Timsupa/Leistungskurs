9b0cee7d8339faf09455dce0857f51779ddec09a
Timsupa <tim.haggerty02@gmail.com>
2026-02-15 01:07:39 +0100

    andere person bewegen

--- Schule/src/blatt12/InsertionSort.java
+++ Schule/src/blatt12/InsertionSort.java
@@ -1,24 +1,23 @@
 package blatt12;
 
 public class InsertionSort {
-    public static int[] insertionSort(int[] arr){
-         int wert = 0;
-        int j = 0;
-        for(int i=1;i<arr.length-1;i++){
-            wert = arr[i];
-            j = i;
-            while(j>0 && arr[j]> wert ){
-                arr[j] = arr[j-1];
-                j = j-1;
-                arr[j]= wert;
+    public static int[] insertionSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            int wert = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > wert) {
+                arr[j+1] = arr[j];
+                j--;
             }
+            arr[j+1] = wert;
         }
         return arr;
     }
-    public static int[] insertionSortInPlace(int[] arr){
-        int[] neu = new int[arr.length];
-        int hochpunkt = 0;
-        for(int j=0;j<arr.length;j++) {
+    /*public static int[] insertionSortInPlace(int[] arr){
+        for(int j = 0; j < arr.length; j++) {
+            int l = arr[j];
+            int obi = j-1;
             for (int i = 0; i < arr.length - 1; i++) {
                 if (arr[i] < arr[i + 1]) {
                     hochpunkt = arr[i];
@@ -28,5 +27,9 @@ public class InsertionSort {
             neu[j] = hochpunkt;
         }
         return neu;
+    }*/
+
+    public static void main(String[] args) {
+        blatt07.ArbeitMitArrays.printArray(insertionSort(new int[]{1,2,12,4,53,6,7,8,9,10}));
     }
 }
--- Schule/src/blatt12/SelectionSort.java
+++ Schule/src/blatt12/SelectionSort.java
@@ -1,19 +1,20 @@
 package blatt12;
 
-public class SelectionSort {
+public class  SelectionSort {
     public static void selectionSort(int[] arr, String mm) {
         if (mm == "min") {
             int min = 0;
-            for (int i = 0; i < arr.length - 1; i++) {
+            for (int i = 0; i < arr.length; i++) {
                 min = i;
-                for (int j = i + 1; j < arr.length; j++) {
+                for (int j = i+1; j < arr.length; j++) {
                     if (arr[j] < arr[min]) {
                         min = j;
                     }
-                    int temp = arr[j];
-                    arr[j] = arr[min];
-                    arr[min] = temp;
+
                 }
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
             }
         } else if (mm == "max") {
             int max = 0;
@@ -23,16 +24,17 @@ public class SelectionSort {
                     if (arr[j] > arr[max]) {
                         max = j;
                     }
-                    int temp = arr[j];
-                    arr[j] = arr[max];
-                    arr[max] = temp;
+
                 }
+                int temp = arr[i];
+                arr[i] = arr[max];
+                arr[max] = temp;
             }
         }
         blatt07.ArbeitMitArrays.printArray(arr);
     }
 
     public static void main(String[] args) {
-        selectionSort(new int[]{1,2,32,4,53,6,7,8,9,10}, "min");
+        selectionSort(new int[]{1,2,12,4,53,6,7,8,9,10}, "max");
     }
 }
