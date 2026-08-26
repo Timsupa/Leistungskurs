1c72e796d7cdb3540c39c410aea7c8a7c62002e0
TimothyHaggerty <tim.haggerty02@gmail.com>
2026-08-26 11:04:16 +0200

    düsterer graph algorithmus michael zu kevin

--- Schule/src/blatt33/aufgabe07/Dijkstra.java
+++ Schule/src/blatt33/aufgabe07/Dijkstra.java
@@ -2,56 +2,57 @@ package blatt33.aufgabe07;
 
 import blatt32.aufgabe03.Graph;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
 public class Dijkstra {
 
-    public void dijkstra(int[][] graph, int start) {
-        int[][] neu = new int[graph.length][graph.length];
-        Stack<Integer> vorgänger = new Stack<>();
-        boolean[] visited = new boolean[graph.length];
-        int[] wege = new int[graph.length];
-        for (int i = 0; i < graph.length; i++) {
-            visited[i] = false;
+    public int getKleinstes(ArrayList<Integer> L,int[][] way) {
+        int min =  Integer.MAX_VALUE;
+        int gn = 0;
+        for(int j=0;j<L.size();j++){
+            int leek = L.get(j);
+            if (way[leek][j]< min){
+                gn = leek;
+                min = way[leek][j];
+            }
         }
-        int h = 0;
-        visited[start] = true;
-
-        while (h != graph.length) {
-            h = 0;
-            int[][] temp = new int[graph.length][graph.length];
-            for (int i = 0; i < graph.length; i++) {
-                if (graph[start][i] <= 1) {
-                    temp[start][i] = graph[start][i];
-                }
-            }
-            int bestWeg = 0;
-            for (int j = 0; j < graph.length - 1; j++) {
-                if (temp[start][j] >= graph[start][j + 1]) {
-                    bestWeg = j+1;
-                    temp[start][j + 1] = graph[start][j];
-                }
-            }
-
-            visited[bestWeg] = true;
-            neu[start][bestWeg] = graph[start][bestWeg];
-            vorgänger.add(start);
-            start = bestWeg;
-
-
+        return gn;
         }
 
-
-
-
-            for (int i = 0; i < graph.length; i++) {
-                if (visited[i] == true) {
-                    h++;
+        public int[][] dijkstra(Graph graph, int start) {
+            ArrayList<Integer> K = new ArrayList<>();
+            boolean[] hausbesuch = new boolean[graph.size()];
+            int[][] wege =  new int[graph.size()][2];
+            wege[start][0] = 0;
+            wege[start][1] = start;
+            for (int i = 1; i < graph.size(); i++) {
+                wege[start][i] = Integer.MAX_VALUE;
+                wege[start][i] = -500;
+                hausbesuch[i] = false;
+            }
+            K.add(start);
+            while (!K.isEmpty()) {
+                Integer x = getKleinstes(K, wege);
+                K.remove(x);
+                hausbesuch[x] = true;
+                int[] rentner = graph.getNeighbours(x);
+                for (int i = 0; i < rentner.length; i++) {
+                    Integer o = i;
+                    if (hausbesuch[i] == false) {
+                        K.add(o);
+                    }
+                    int neuzeit = wege[o][0];
+                    int barok = wege[x][0];
+                    if (neuzeit < barok) {
+                        wege[o][0] = neuzeit;
+                        wege[o][1] = x;
+                    }
                 }
             }
-
+            return wege;
         }
     }
-}
+
